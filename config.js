24d700f52332b54d7e6adf194306a9a42cab0d67
thanhdat2002 <80498296+thanhdat2002@users.noreply.github.com>
2022-12-25 21:49:26 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Em';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Merry christmars';
+const titleCard = 'Dear đến em ';
+const contentCard = 'Chúc honey của anh một Noel tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+
+const giftImage = 'baonhien.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
